590c7741dfba6fba0a3b8cff1a78382014c60fcf
Pedro Palmer <pedropal98@gmail.com>
2021-11-16 14:53:37 +0100

    reservations of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'studios#index'
+
+  # studios
   get "studios/index", to: "studios#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   get '/studios/:id', to: 'studios#show', as: :studio
+
+  resources :studios do
+    resources :reservations
+  end
 end
