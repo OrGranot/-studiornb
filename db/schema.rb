d716e90349e63225f8a47bb26601948ddd2689ca
or granot <orgranot91@gmail.com>
2021-11-16 14:51:05 +0100

    nowwww

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_133355) do
+ActiveRecord::Schema.define(version: 2021_11_16_134943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_16_133355) do
   create_table "reservations", force: :cascade do |t|
     t.date "start_on"
     t.date "end_on"
-    t.boolean "aproved"
+    t.boolean "approved"
     t.bigint "studio_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
