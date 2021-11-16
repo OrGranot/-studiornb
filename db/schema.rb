d89de991cfdb8551a17754efa5a19bfe48e95835
Philipp Krink <krink.philipp@gmail.com>
2021-11-16 18:05:57 +0100

    -migrated

--- db/schema.rb
+++ db/schema.rb
@@ -36,13 +36,6 @@ ActiveRecord::Schema.define(version: 2021_11_16_163041) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "reservations", force: :cascade do |t|
     t.date "start_on"
     t.date "end_on"
