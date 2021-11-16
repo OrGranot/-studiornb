3d318d75f8ce8f51f199cccc69765fc5299a4271
or granot <orgranot91@gmail.com>
2021-11-16 14:47:53 +0100

    maybe now

--- db/migrate/20211116133355_rename_aprroved_in_reservations.rb
+++ db/migrate/20211116133355_rename_aprroved_in_reservations.rb
@@ -1,5 +1,5 @@
 class RenameAprrovedInReservations < ActiveRecord::Migration[6.0]
   def change
-    rename_column :reservations, :aprroved, :approved
+    rename_column :reservations, :aproved, :approved
   end
 end
