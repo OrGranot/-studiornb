97a20d053d871f01e7d880396b102a0f9e0584ff
Philipp Krink <krink.philipp@gmail.com>
2021-11-16 14:05:15 +0100

    changes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,15 @@
 class ReservationsController < ApplicationController
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_on, :end_on, :studio_id, :user_id, :approved)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'studios#index'
   get "studios/index", to: "studios#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   get '/studios/:id', to: 'studios#show', as: :studio
+  resources :studios do
+    resources :reservations
+  end
 end
