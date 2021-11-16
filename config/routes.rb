534814656a98d331082456fbff037fd2c0501814
Philipp Krink <krink.philipp@gmail.com>
2021-11-16 16:54:37 +0100

    User Dashboard implemented

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'studios#index'
   get "studios", to: "studios#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   resources :studios do
     resources :reservations
   end
+  get "dashboards", to: "dashboards#show"
 end
